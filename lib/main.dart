933841267d319955104d50f9f9fb1a495f802533
Kazuki Ishibashi <s_151002@yahoo.co.jp>
2021-11-22 16:29:49 +0900

    api実行、apiの結果を元に画面遷移

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,35 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _zipApi() async {
+    final url =
+        Uri.parse('https://zipcloud.ibsnet.co.jp/api/search?zipcode=0010000');
+    final response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+  }
+
+  Future<void> _zipApiPageChange() async {
+    final url =
+    Uri.parse('https://zipcloud.ibsnet.co.jp/api/search?zipcode=0010000');
+    final response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    await Navigator.push(
+      context,
+      MaterialPageRoute<void>(
+        builder: (context) => NextPage('Response status: ${response.statusCode}'),
+      ),
+    );
+  }
+
+  Future<void> _localAPITest() async {
+    final url = Uri.parse('http://192.168.0.4:30000/api/v1/taste_types');
+    final response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+  }
+
   final myController = TextEditingController();
 
   @override
@@ -91,15 +120,28 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             ElevatedButton(
-              onPressed: () async {
-                final url = Uri.parse(
-                    'https://zipcloud.ibsnet.co.jp/api/search?zipcode=0010000');
-                final response = await http.get(url);
-                print('Response status: ${response.statusCode}');
-                print('Response body: ${response.body}');
+              onPressed: _zipApi,
+              child: const Text('郵便番号API'),
+            ),
+            ElevatedButton(
+              onPressed: _zipApiPageChange,
+              child: const Text('郵便番号API画面遷移有り'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (context) => NextPage('夏目漱石'),
+                  ),
+                );
               },
-              child: const Text('APIテスト'),
-            )
+              child: const Text('画面遷移'),
+            ),
+            ElevatedButton(
+              onPressed: _localAPITest,
+              child: const Text('ローカルAPIテスト'),
+            ),
           ],
         ),
       ),
