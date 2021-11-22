8e6d27334896efaba38cb7c97a674dbc3a2834fe
Kazuki Ishibashi <s_151002@yahoo.co.jp>
2021-11-22 19:30:05 +0900

    api関連処理　ファイル分割

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sample_01/next_page.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_sample_01/service/api.dart';
 
 void main() {
   runApp(const MyApp());
@@ -43,6 +43,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   String dropdownValue = 'One';
+  ApiService api = ApiService();
 
   void _incrementCounter() {
     setState(() {
@@ -54,37 +55,6 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
-
-  void _zipApi() async {
-    final url =
-        Uri.parse('https://zipcloud.ibsnet.co.jp/api/search?zipcode=0010000');
-    final response = await http.get(url);
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-  }
-
-  Future<void> _zipApiPageChange() async {
-    final url =
-        Uri.parse('https://zipcloud.ibsnet.co.jp/api/search?zipcode=0010000');
-    final response = await http.get(url);
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-    await Navigator.push(
-      context,
-      MaterialPageRoute<void>(
-        builder: (context) =>
-            NextPage('Response status: ${response.statusCode}'),
-      ),
-    );
-  }
-
-  Future<void> _localAPITest() async {
-    final url = Uri.parse('http://192.168.0.4:30000/api/v1/taste_types');
-    final response = await http.get(url);
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-  }
-
 
   final myController = TextEditingController();
 
@@ -123,13 +93,13 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             ElevatedButton(
-              onPressed: _zipApi,
+              onPressed: api.zipApi,
               child: const Text('郵便番号API'),
             ),
-            ElevatedButton(
-              onPressed: _zipApiPageChange,
-              child: const Text('郵便番号API画面遷移有り'),
-            ),
+            // ElevatedButton(
+            //   onPressed: _zipApiPageChange,
+            //   child: const Text('郵便番号API画面遷移有り'),
+            // ),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -142,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
               child: const Text('画面遷移'),
             ),
             ElevatedButton(
-              onPressed: _localAPITest,
+              onPressed: api.localAPITest,
               child: const Text('ローカルAPIテスト'),
             ),
             DropdownButton<String>(
---
+++ lib/service/api.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  Future<void> zipApi() async {
+    final url =
+    Uri.parse('https://zipcloud.ibsnet.co.jp/api/search?zipcode=0010000');
+    final response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+  }
+
+  Future<void> zipApiPageChange() async {
+    final url =
+    Uri.parse('https://zipcloud.ibsnet.co.jp/api/search?zipcode=0010000');
+    final response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    // await Navigator.push(
+    //   context,
+    //   MaterialPageRoute<void>(
+    //     builder: (context) =>
+    //         NextPage('Response status: ${response.statusCode}'),
+    //   ),
+    // );
+  }
+
+  Future<void> localAPITest() async {
+    final url = Uri.parse('http://192.168.0.4:30000/api/v1/taste_types');
+    final response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+  }
+}
