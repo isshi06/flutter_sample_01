3b49e7fe75eb33f3763883973d3ff4935ad1b131
Kazuki Ishibashi <s_151002@yahoo.co.jp>
2021-11-24 11:00:37 +0900

    Riverpodサンプル

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sample_01/next_page.dart';
 import 'package:flutter_sample_01/service/api.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,130 +22,58 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ProviderPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+// Provider example.
 
-  // このウィジェットは、アプリケーションのホームページです。ステートフル、つまり
-  // 影響を与えるフィールドを含むStateオブジェクト（以下に定義）があること
-  // それがどのように見えるか。
+// 1ずつ値を増加させるためのカウンターProvider
+final counterProvider = StateProvider((ref) => 0);
 
-  // このクラスは、状態の構成です。それは値を保持します（これでは
-  // 親（この場合はアプリウィジェット）によって提供されたタイトル）と
-  // stateのビルドメソッドで使用されます。ウィジェットサブクラスのフィールドは次のとおりです。
-  // 常に「最終」とマークされます。
+// カウンターの値を2倍にした値を提供するProvider
+final doubleCounterProvider = Provider((ref) {
+  final count = ref.watch(counterProvider);
+  return count * 2;
+});
 
-  final String title;
+// Widget example.
+class ProviderPage extends ConsumerWidget {
+  const ProviderPage({Key? key}) : super(key: key);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String dropdownValue = 'One';
-  ApiService api = ApiService();
-
-  void _incrementCounter() {
-    setState(() {
-      // このsetStateの呼び出しは、Flutterフレームワークに何かが持っていることを伝えます
-      // この状態で変更されたため、以下のビルドメソッドが再実行されます
-      // 表示に更新された値を反映できるようにします。変更した場合
-      // setState（）を呼び出さずに_counterを実行すると、ビルドメソッドは
-      // 再度呼び出されたため、何も起こらなかったようです。
-      _counter++;
-    });
-  }
-
-  final myController = TextEditingController();
+  static const String title = 'ProviderPage';
+  static const String routeName = 'provider-page';
 
   @override
-  Widget build(BuildContext context) {
-    // このメソッドは、たとえば完了したように、setStateが呼び出されるたびに再実行されます。
-    // 上記の_incrementCounterメソッドによって。
+  Widget build(BuildContext context, WidgetRef ref) {
+    // doubleCounterProvider を読み取る。
+    // counterProvider の状態が更新されると doubleCounterProvider も変更され、再構築される。
+    final doubleCount = ref.watch(doubleCounterProvider);
+
     return Scaffold(
-      appBar: AppBar(
-        // ここでは、によって作成されたMyHomePageオブジェクトから値を取得します
-        // App.buildメソッドを使用し、それを使用してアプリバーのタイトルを設定します。
-        title: Text(widget.title),
+      appBar: AppBar(title: const Text(title)),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            '2倍されたカウント値：',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          // doubleCounterProvider の値を表示
+          Text(
+            '$doubleCount',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          ElevatedButton(
+            // counterProvider の値を+1する。
+            onPressed: () => ref.read(counterProvider.notifier).update(
+                  (state) => state + 1,
+            ),
+            child: const Text('Increase Count'),
+          ),
+        ],
       ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          children: [
-            TextField(
-              decoration: const InputDecoration(
-                hintText: '名前',
-              ),
-              onChanged: (text) {
-                print('First text field: $text');
-              },
-              controller: myController,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                hintText: '趣味',
-              ),
-            ),
-            ElevatedButton(
-              child: const Text('新規登録'),
-              onPressed: () {
-                // TODO: 新規登録
-              },
-            ),
-            ElevatedButton(
-              onPressed: api.zipApi,
-              child: const Text('郵便番号API'),
-            ),
-            // ElevatedButton(
-            //   onPressed: _zipApiPageChange,
-            //   child: const Text('郵便番号API画面遷移有り'),
-            // ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute<void>(
-                    builder: (context) => NextPage('夏目漱石'),
-                  ),
-                );
-              },
-              child: const Text('画面遷移'),
-            ),
-            ElevatedButton(
-              onPressed: api.localAPITest,
-              child: const Text('ローカルAPIテスト'),
-            ),
-            DropdownButton<String>(
-              value: dropdownValue,
-              icon: const Icon(Icons.arrow_downward),
-              iconSize: 24,
-              elevation: 16,
-              style: const TextStyle(color: Colors.deepPurple),
-              underline: Container(
-                height: 2,
-                color: Colors.deepPurpleAccent,
-              ),
-              onChanged: (String? newValue) {
-                setState(() {
-                  dropdownValue = newValue!;
-                });
-              },
-              items: <String>['One', 'Two', 'Free', 'Four']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-          ],
-        ),
-      ), // この末尾のコンマにより、ビルドメソッドの自動フォーマットがより適切になります。
     );
   }
 }
